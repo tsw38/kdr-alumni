c6e98658276419c3d8fbfa0f631a74f1b36b2a41
Tyler Williams <tsw38@cornell.edu>
2019-01-15 21:31:27 -0600

    Add sql dump and plugins directory

--- js/resize.js
+++ js/resize.js
@@ -5,20 +5,22 @@ var currentFontSize;
 
 // window.resize callback function
 function setBodyFont() {
-    if(window.innerWidth >= shrinkDimension && typeof currentFontSize !== undefined) {
-        currentFontSize = undefined;
-
-        Array.from(document.querySelectorAll('html, body')).forEach(function(node, index) {
-            node.style.fontSize = '';
-        });
-    }
-
-    if(window.innerWidth < shrinkDimension) {
-        currentFontSize = (maxFontSize/shrinkDimension) * window.innerWidth;
-        Array.from(document.querySelectorAll('html, body')).forEach(function(node, index) {
-            node.style.fontSize = currentFontSize + 'px';
-        });
-    }
+    setTimeout(function(){
+        if(window.innerWidth >= shrinkDimension && typeof currentFontSize !== undefined) {
+            currentFontSize = undefined;
+    
+            Array.from(document.querySelectorAll('html, body')).forEach(function(node, index) {
+                node.style.fontSize = '';
+            });
+        }
+    
+        if(window.innerWidth < shrinkDimension) {
+            currentFontSize = (maxFontSize/shrinkDimension) * window.innerWidth;
+            Array.from(document.querySelectorAll('html, body')).forEach(function(node, index) {
+                node.style.fontSize = currentFontSize + 'px';
+            });
+        }
+    }, 0);
 }
 
 // window.resize event listener

56373050121247a38ec3d8f22e0ef0461516f959
Tyler Williams <tsw38@cornell.edu>
2019-01-16 19:10:21 -0600

    Updating header styling for mobile:

--- functions-api.php
+++ functions-api.php
@@ -93,4 +93,23 @@
 
         return $factsArr[array_rand($factsArr)];
     }
+
+    function generateArticle($article){
+        
+        echo('<div class="Article">
+            <h5 class="Article-title">'.$article['post_title'].'</h5>
+            <div class="row">
+                <div class="Article-image" style="background-image: url('.get_the_post_thumbnail_url($article['ID']).'); ">
+                </div>
+                <div class="Article-description">
+                    <div>'
+                        . get_fields($article['ID'])['brief_description'] .
+                    '</div>
+                    <div class="viewMore">
+                        <a href="' . $article["guid"] . '">View More &gt;</a>
+                    </div>'.
+                '</div>
+            </div>
+        </div>');
+    }
 ?>
--- helpers.php
+++ helpers.php
@@ -1,21 +1,5 @@
 <?php
 
-    function generateArticle($article){
-        echo('<div class="Article">
-            <h5 class="Article-title">'.$article['post_title'].'</h5>
-            <div class="row">
-                <div class="Article-image" style="background-image: url('.get_the_post_thumbnail_url($article['ID']).'); ">
-                </div>
-                <div class="Article-description">
-                    <div>'
-                        . $article['post_content'] .
-                    '</div>
-                    <div class="viewMore">
-                        <a href="' . $article["guid"] . '">View More &gt;</a>
-                    </div>'.
-                '</div>
-            </div>
-        </div>');
-    }
+    
 
 ?>
